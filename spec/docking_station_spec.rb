c7747f59e479b2fb52aa860ca5a33fcb5f2bd304
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-05-13 15:44:10 +0100

    modified lib and spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+ it "responds to release bike method" do
+   docking_station = DockingStation.new
+   docking_station.release_bike
 
+   expect(docking_station).to respond_to(release_bike)
+
+ end
 end
