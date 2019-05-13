f69f04d5a1fc4b4643158d0d2525c208aa5d0144
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-13 16:07:52 +0100

    test for working? passes with true.  Created Bike class.

---
+++ spec/bike_spec.rb
@@ -0,0 +1,9 @@
+require 'bike.rb'
+
+describe Bike do 
+    it 'responds to working? method' do 
+        bike = Bike.new
+        
+        expect(bike).to respond_to(:working?)
+    end 
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,16 @@ require 'docking_station.rb'
 
 describe DockingStation do
  it "responds to release bike method" do
-   docking_station = DockingStation.new
-   docking_station.release_bike
+	docking_station = DockingStation.new
+	docking_station.release_bike
 
-   expect(docking_station).to respond_to(release_bike)
+	expect(docking_station).to respond_to(:release_bike)
 
  end
+
+	it "gets a bike and expects it to be working" do 
+		bike = Bike.new
+
+		expect(bike.working?).to eq(true)
+	end
 end
