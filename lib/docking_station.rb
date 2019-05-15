2d6cd15bf4ff0c65318d0dfbf29d93d050979e4f
Egle Bernotaite <ebernotaite94@gmail.com>
2019-05-15 15:41:10 +0100

    Step 13 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,20 @@
 class DockingStation
   attr_reader :bike
-  
+  def initialize
+    @bike = nil
+  end
+
   def release_bike
     if @bike == nil
-      raise "this is an error"
+      raise "there is no bike to release"
     else
-    Bike.new
+      bike_to_return = @bike
+      @bike = nil
+      bike_to_return
     end
   end
   
   def dock(bike)
-    @bike = bike
+    @bike ? raise("there is a bike docked already") : @bike = bike
   end
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,43 +1,29 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-#  it "responds to release bike method" do
-# 	docking_station = DockingStation.new
-# 	docking_station.release_bike
-
-# 	expect(docking_station).to respond_to(:release_bike)
-
-#  end
+  let(:station){DockingStation.new}
+  let(:bike){Bike.new}
 
 	it "gets a bike and expects it to be working" do
-		bike = Bike.new
-
 		expect(bike.working?).to eq(true)
 	end
 
-	
   it "can dock a bike" do
-		station = DockingStation.new
-		bike = Bike.new
-
-    expect(station.dock(bike)).to eq(bike)
+		expect(station.dock(bike)).to eq(bike)
 	end
 
 
 	it "can show a docked bike" do
-		docking_station = DockingStation.new
-		bike = Bike.new
-
-		docking_station.dock(bike)
-		
-		expect(docking_station.bike).to eq(bike)
+	  station.dock(bike)
+		expect(station.bike).to eq(bike)
   end
   
   it 'can throw error if no bike is docked' do
-    station = DockingStation.new
-    station.dock(nil)
-    expect{station.release_bike}.to raise_error("this is an error")
+    expect{station.release_bike}.to raise_error("there is no bike to release")
   end
 
-
+  it 'can throw error if there is a bike docked already' do
+    station.dock(bike)
+    expect{station.dock(bike)}.to raise_error("there is a bike docked already")
+  end
 end
